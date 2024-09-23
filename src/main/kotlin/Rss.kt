dcfcc5ef3214e25b4eb9c14fc760247897e990a6
hengzizng <hengzizng@naver.com>
2024-09-23 14:49:17 +0900

    feat: 기술블로그 N개 글을 읽어온다.

--- src/main/kotlin/Rss.kt
+++ src/main/kotlin/Rss.kt
@@ -1,3 +1,4 @@
+import org.w3c.dom.Document
 import org.w3c.dom.Element
 import org.w3c.dom.Node
 import org.w3c.dom.Node.ELEMENT_NODE
@@ -8,12 +9,24 @@ import java.io.StringReader
 import java.net.URL
 import javax.xml.parsers.DocumentBuilderFactory
 
+// 우아한 형제들: https://techblog.woowahan.com/feed
+// 라인: https://techblog.lycorp.co.jp/ko/feed/index.xml
+// 뱅크샐러: https://blog.banksalad.com/rss.xml
 
 class Rss {
-    fun getElenmetByTagName() : Node{
+    fun getElenmetByTagName() {
         val factory = DocumentBuilderFactory.newInstance()
-        val xml = factory.newDocumentBuilder()
-            .parse("https://techblog.woowahan.com/feed")
+        val xml = factory.newDocumentBuilder().parse("https://techblog.woowahan.com/feed")
+        parseArticle(xml)
+
+        val xml2 = factory.newDocumentBuilder().parse("https://techblog.lycorp.co.jp/ko/feed/index.xml")
+        parseArticle(xml2)
+
+        val xml3 = factory.newDocumentBuilder().parse("https://blog.banksalad.com/rss.xml")
+        parseArticle(xml3)
+    }
+
+    private fun parseArticle(xml: Document): Node? {
         val channel = xml.getElementsByTagName("channel").item(0)
         val channelElement = channel as Element
         val items = channelElement.getElementsByTagName("item")
@@ -27,7 +40,6 @@ class Rss {
                 println(itemElement.getElementsByTagName("description").item(0).textContent)
             }
         }
-
         return channel
     }
 }
