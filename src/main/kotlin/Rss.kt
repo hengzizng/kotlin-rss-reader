6bffc49d9d50514b8540de59b7b46d0eabf76187
hengzizng <hengzizng@naver.com>
2024-09-23 16:33:42 +0900

    feat: 기본 기능 요구사항 구현

--- src/main/kotlin/Rss.kt
+++ src/main/kotlin/Rss.kt
@@ -2,11 +2,9 @@ import org.w3c.dom.Document
 import org.w3c.dom.Element
 import org.w3c.dom.Node
 import org.w3c.dom.Node.ELEMENT_NODE
-import org.xml.sax.InputSource
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.io.StringReader
-import java.net.URL
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 import javax.xml.parsers.DocumentBuilderFactory
 
 // 우아한 형제들: https://techblog.woowahan.com/feed
@@ -14,6 +12,7 @@ import javax.xml.parsers.DocumentBuilderFactory
 // 뱅크샐러: https://blog.banksalad.com/rss.xml
 
 class Rss {
+    val articleList = mutableListOf<Article>()
     fun getElenmetByTagName() {
         val factory = DocumentBuilderFactory.newInstance()
         val xml = factory.newDocumentBuilder().parse("https://techblog.woowahan.com/feed")
@@ -27,19 +26,62 @@ class Rss {
     }
 
     private fun parseArticle(xml: Document): Node? {
+        val dateFormat = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss Z", Locale.ENGLISH)
+
         val channel = xml.getElementsByTagName("channel").item(0)
         val channelElement = channel as Element
         val items = channelElement.getElementsByTagName("item")
+
         for (i in 0 until items.length) {
             val itemNode = items.item(i)
             if (itemNode.nodeType == ELEMENT_NODE) {
                 val itemElement = itemNode as Element
-
-                println(itemElement.getElementsByTagName("title").item(0).textContent)
-                println(itemElement.getElementsByTagName("link").item(0).textContent)
-                println(itemElement.getElementsByTagName("description").item(0).textContent)
+                val article = Article(
+                    LocalDateTime.parse(itemElement.getElementsByTagName("pubDate").item(0).textContent, dateFormat),
+                    itemElement.getElementsByTagName("title").item(0).textContent,
+                    itemElement.getElementsByTagName("link").item(0).textContent,
+                    itemElement.getElementsByTagName("description").item(0).textContent
+                )
+                articleList.add(article)
             }
         }
         return channel
     }
+
+    fun sortArticleByDate() {
+        articleList.sortByDescending { it.date }
+    }
+
+    fun printArticle(number: Int) {
+        articleList.take(number).forEach {
+            println(it.date)
+            println(it.link)
+            println(it.title)
+            println(it.description)
+        }
+    }
+    fun printArticle(article: List<Article>) {
+        article.forEach {
+            println(it.date)
+            println(it.link)
+            println(it.title)
+            println(it.description)
+        }
+    }
+
+    fun findArticleByWord(word:String) : List<Article> {
+        val findArticle = articleList.filter { it.title!!.contains(word) }
+        return findArticle
+    }
+}
+
+class Article(
+    //val date : Date ,
+    val date : LocalDateTime,
+    val title : String? ,
+    val link : String? ,
+    val description: String? ,
+){
+
+
 }
--- src/test/kotlin/RssTest.kt
+++ src/test/kotlin/RssTest.kt
@@ -5,7 +5,12 @@ class RssTest {
     @Test
     fun `데이터가 있다`() {
         val rss = Rss()
-        val data = rss.getElenmetByTagName()
-        println(data)
+       rss.getElenmetByTagName()
+        rss.articleList
+        rss.sortArticleByDate()
+        //rss.articleList
+        //rss.printArticle(10)
+        val findArticle = rss.findArticleByWord("데이터")
+        rss.printArticle(findArticle)
     }
 }
